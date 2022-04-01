c9c99c6a72d74db7d658d7873bdc69c03dfa9344
LastRonin69 <matteomenicacci97@gmail.com>
2022-04-01 13:41:14 +0200

    Update divIstogramma.js

--- Esercitazione4/divIstogramma.js
+++ Esercitazione4/divIstogramma.js
@@ -2,18 +2,16 @@
 d3.csv("Istat-popolazioneResidentePerRegione2021.csv", function(dati){
     console.log(dati);
     const larghezzaBarre = Math.floor(100/dati.length);
-    
     const valoreMassimo = Number(dati[0].Value);
     document.getElementById("aggiornamento").innerHTML = 
     dati[0]["Tipo di indicatore demografico"]+" "+dati[0].TIME;
 d3.select("figure")
 .selectAll("div")
-.data("dati")
+.data(dati)
 .enter()
 .append("div")
 .style("width", `${larghezzaBarre}%`)
 .style("height", function(d){
-    
     let altezzaBarra = Math.round(d.Value / valoreMassimo * 100)
     return `${altezzaBarra}%`
 })
@@ -24,26 +22,27 @@ d3.select("figure")
 }) */
 
 "use strict" /* obbliga a dichiarare tutte le variabili utilizzate */
-d3.csv("Istat-popolazioneResidentePerRegione2021.csv", function(dati) {
-console.log(dati);
-const larghezzaBarre = Math.floor(100/dati.length);
-/* il valore massimo è il totale dell'Italia */
-const valoreMassimo = Number(dati[0].Value);
-document.getElementById("aggiornamento").innerHTML =
-dati[0]["Tipo di indicatore demografico"]+" "+dati[0].TIME;
-d3.select("figure")
-.selectAll("div")
-.data(dati)
-.enter()
-.append("div")
-.style("width",`${larghezzaBarre}%`)
-.style("height",function(d) {
-/* rapporta l'altezza al massimo */
-let altezzaBarra = Math.round(d.Value / valoreMassimo * 100)
-return `${altezzaBarra}%`
+d3.csv("Istat-popolazioneResidentePerRegione2021.csv", function (dati) {
+    console.log(dati);
+    const larghezzaBarre = Math.floor(100 / dati.length);
+    /* il valore massimo è il totale dell'Italia */
+    const valoreMassimo = Number(dati[0].Value);
+    document.getElementById("aggiornamento").innerHTML =
+        dati[0]["Tipo di indicatore demografico"] + " " + dati[0].TIME;
+    d3.select("figure")
+        .selectAll("div")
+        .data(dati)
+        .enter()
+        .append("div")
+        .style("width", `${larghezzaBarre}%`)
+        .style("height", function (d) {
+            /* rapporta l'altezza al massimo */
+            let altezzaBarra = Math.round(d.Value / valoreMassimo * 100)
+            return `${altezzaBarra}%`
+        })
+        .attr("title", function (d) {
+            return `${d.Territorio}: ${Number(d.Value).toLocaleString()}`
+            /* Number() converte la stringa in numero e toLocaleString() scrive il numero con le migliaia */
 })
-.attr("title", function(d) {
-return `${d.Territorio}: ${Number(d.Value).toLocaleString()}`
-/* Number() converte la stringa in numero e toLocaleString() scrive il numero con le migliaia */ })
 
 });
